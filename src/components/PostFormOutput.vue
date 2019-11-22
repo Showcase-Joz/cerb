0432a3623461e064f400fedc1042447d68f1a006
Joz <sunrise.joz77@gmail.com>
2019-11-22 13:25:39 +0000

    outputting data to another sub component of post.vue

---
+++ src/components/PostFormOutput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>test
+
+    <p>{{this.passedPost}}</p>
+  </div>
+  
+</template>
+<script>
+export default {
+  name: "PostFormOutput",
+  data() {
+    return {
+      newPost: {}
+    }
+  },
+  props: {
+    passedPost: {
+      type: Object
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,35 +1,31 @@
 <template>
   <div class="post-view">
     <h1>This is a POST page</h1>
-    <p>
-      Please complete this form to POST data to the API for testing purposes
-    </p>
+    <p>Please complete this form to POST data to the API for testing purposes</p>
     <PostForm v-on:handlePost="retainPost" />
+    <PostFormOutput v-bind:passedPost="passedPost" />
   </div>
 </template>
 
 <script>
 import PostForm from "../components/form/PostForm";
-let dummy = {}
-console.log(dummy);
+import PostFormOutput from "../components/PostFormOutput";
 
 export default {
   name: "post",
   data() {
     return {
-      passedPost: dummy
-    }
+      passedPost: {}
+    };
   },
   components: {
-    PostForm
+    PostForm,
+    PostFormOutput
   },
   methods: {
     retainPost(newPost) {
       console.info("test: ", newPost);
       this.passedPost = newPost;
-      console.log("passed: ", this.passedPost);
-      console.log("passed: ", this.passedPost.type);
-      
     }
   }
 };
