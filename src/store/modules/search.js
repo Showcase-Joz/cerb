4819d51914c408bd4cdadf6d5cf04439a70bb2cb
Joz <jolyon.favreau@sst-l.com>
2020-02-25 15:09:40 +0000

    added search vuex module

---
+++ src/store/modules/search.js
@@ -0,0 +1,24 @@
+export const namespaced = true;
+
+export const state = {
+  searchedContent: ""
+};
+
+export const getters = {
+  searchedContent: state => {
+    return state.searchedContent;
+  }
+}
+
+export const mutations = {
+  SEARCH_STRING(state, string) {
+    state.searchedContent = string;
+  }
+}
+
+export const actions = {
+  storedSearch({ commit, dispatch }, payload) {    
+    commit("SEARCH_STRING", payload);
+    dispatch('authorisation/testAction', "steve", { root: true })
+  }
+}
