18ad782d974ecfa32288a1559218d97806f39189
Jolyon Favreau <joz@Jolyons-MacBook-Pro.local>
2020-03-03 14:16:44 +0000

    updated s3-deploy config

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,28 @@ module.exports = {
     }
   },
 
-  productionSourceMap: false
+  productionSourceMap: false,
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: "default",
+      overrideEndpoint: false,
+      region: "eu-west-1",
+      bucket: "typhon-ui",
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: "index.html",
+      staticErrorPage: "index.html",
+      assetPath: "dist",
+      assetMatch: "**",
+      deployPath: "/",
+      acl: "authenticated-read",
+      pwa: true,
+      pwaFiles: "index.html,service-worker.js,manifest.json",
+      enableCloudfront: false,
+      pluginVersion: "4.0.0-rc3",
+      uploadConcurrency: 5
+    }
+  }
 };
