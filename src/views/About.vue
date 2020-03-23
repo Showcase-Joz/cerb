f021c7ee4e14160a997b822d4bc05668d506d11a
Jolyon Favreau <joz@Jolyons-MacBook-Pro.local>
2020-03-23 17:49:17 +0000

    outlined about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about page">
-    <h1>This is an about page</h1>
+    <h1>About Typhon UI</h1>
+    <br >
+    <h3>Development Log</h3>
+    <div class="log-wrapper">
+      <ul>
+        <li>this is a log item 1</li>
+        <li>this is a log item 2</li>
+        <li>this is a log item 3</li>
+        <li>this is a log item 4</li>
+        <li>this is a log item 5</li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.log-wrapper {
+	display: grid;
+	grid-template-columns: 0.8fr;
+	justify-content: center;
+	padding-top: 5rem;
+
+	ul {
+    counter-reset: update;
+    list-style-type: none;
+
+		li {
+			border-bottom: $color2 dashed 1px;
+			border-bottom-left-radius: 15px;
+			border-top-left-radius: 20px;
+			border-left: green solid 3px;
+			margin-bottom: $spacingDefault;
+      padding: $spacingDefault calc(#{$spacingDefault} / 2);
+      text-align: start;
+			width: 100%;
+
+			&::before {
+				counter-increment: update;
+        content: counter(update);
+        left: -20px;
+        position: absolute;
+			}
+		}
+	}
+}
+</style>
